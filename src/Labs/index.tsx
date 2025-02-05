84cf257e80ebf7a7a6befb4e7a6d33e5a5c4de32
Aaron093 <97973556+Aaron093@users.noreply.github.com>
2025-02-05 04:20:03 -0500

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,7 +8,7 @@ export default function Labs() {
     <div>
       <h1>Labs</h1>
       <p>Yiqian Zhang  CS4550.37031.202530</p>
-      <a href="https://yiqian-zhang-cs4550.netlify.app/#/Kambaz/Account/Signin" id="wd-kambaz">Kambaz Application</a>
+      <a href="https://yiqian-zhang-lab2.netlify.app/#/Kambaz/Account/Signin" id="wd-kambaz">Kambaz Application</a>
       <br></br>
       <a href="https://github.com/Aaron093/kambaz-react-web-app" id="wd-github">Github Repo</a>
 
