920aa4db32b47afa218abea3dae5b762f1b1acf1
Aaron093 <zhang.yiqia@northeastern.edu>
2025-03-12 00:06:59 +0800

    Update navigation to satisfy the requirement

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,17 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-
-
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
-  return (
-    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kambaz/Account/Signin" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Signin </Link>
-      <Link to="/Kambaz/Account/Signup" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Signup </Link>
-      <Link to="/Kambaz/Account/Profile" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Profile</Link>
-    </div>
+ const { currentUser } = useSelector((state: any) => state.accountReducer);
+ const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+ const active = (path: string) => (pathname.includes(path) ? "active" : "");
+ const { pathname } = useLocation();
+ return (
+   <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+     {links.map((link) => (
+       <Link key={link} to={`/Kambaz/Account/${link}`} className={`list-group-item border border-0 ${active(link)}`}> {link} </Link>
+     ))}
+   </div>
 );}
